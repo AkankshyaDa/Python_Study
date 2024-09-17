b9c144a6da10a1dc4e66eeccfd11302da03c2df0
Akankshya Dash <akankshyadash3@gmail.com>
2024-09-17 22:16:35 +0530

    Created one folder for testing

---
+++ Python-code/16-09-2024_RuleDemo.py
@@ -0,0 +1,25 @@
+'''
+l = 10
+m = 20
+rule = input("have u broken the rule yes/no?")
+if rule == 'yes':
+    type = int(input('which rule 1 or 2?'))
+    if type == 1:
+        print(f" fine is {l} rupees")
+    elif type == 2:
+        print(f" fine is {m} rupees")
+    else:
+        print("Please enter a valid number 1 or 2")
+else:
+    print("Please enter yes or no")
+'''
+#
+int
+float
+str
+bool
+
+
+
+
+
